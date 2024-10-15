54faa096c2f405953fb52d5ad09b4f75b31e671a
SHALINGA MANASINGHE <shalingams@gmail.com>
2024-10-15 21:47:24 +0300

    adding File clean up

--- backend.py
+++ backend.py
@@ -1,22 +1,23 @@
 import os
 import magic
+import shutil
 
 mime = magic.Magic(mime=True)
 
-files_with_types:dict[str, str] = {}
+files_with_types: dict[list, list] = {}
 
 def cleaner(path: str):
-    create_file_list(path)
+    clean_up(path)
     
     
     
-def create_file_list(path: str):
-    dir_list = os.listdir(path)
-
+def clean_up(path: str):
+    dir_list = (
+        file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))
+    )
     for file in dir_list:
-        file_type = mime.from_file(f'{path}/{file}')
-        files_with_types[file_type] = file
-        # print(os.path.splitext(file))
-    
-    print(files_with_types)   
-    return "Done"
+        file_type = mime.from_file(f"{path}/{file}")
+        if not os.path.exists(f'{path}/{file_type}'):
+            os.makedirs(f"{path}/{file_type}")
+            
+        shutil.move(f"{path}/{file}", f"{path}/{file_type}/{file}")
